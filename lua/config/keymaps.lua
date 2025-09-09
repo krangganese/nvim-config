9e4e0a9d0281620aff48deb95320344e5a0cb874
Dandu Pratama <gondronglabs@gmail.com>
2025-09-09 08:54:30 +0700

    feat: add eslint --fix command

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,13 +4,16 @@ local fzf = require("fzf-lua")
 
 -- Telescope
 keymap("n", "<leader>ff", fzf.files, { desc = "Fzf find files in current working directory" })
-keymap("n", "<leader>fg", fzf.live_grep,  { desc = "Fzf Live grep current project" })
-keymap("n", "<leader>fb", fzf.buffers,    { desc = "Fzf file buffers" })
-keymap("n", "<leader>fh", fzf.helptags,  { desc = "Fzf help tags" })
-keymap("n", "<leader>fk", fzf.keymaps,  { desc = "Fzf keymaps" })
-keymap("n", "<leader>fw", fzf.grep_cword,  { desc = "Fzf find current word" })
-keymap("n", "<leader>fk", fzf.grep_cWORD,  { desc = "Fzf find current WORD" })
+keymap("n", "<leader>fg", fzf.live_grep, { desc = "Fzf Live grep current project" })
+keymap("n", "<leader>fb", fzf.buffers, { desc = "Fzf file buffers" })
+keymap("n", "<leader>fh", fzf.helptags, { desc = "Fzf help tags" })
+keymap("n", "<leader>fk", fzf.keymaps, { desc = "Fzf keymaps" })
+keymap("n", "<leader>fw", fzf.grep_cword, { desc = "Fzf find current word" })
+keymap("n", "<leader>fk", fzf.grep_cWORD, { desc = "Fzf find current WORD" })
 keymap("n", "<C-p>", fzf.git_files, { desc = "Fzf git files" })
+
+-- TS / JS
+keymap("n", "<leader>ef", ":w<CR>:!npx eslint --fix %<CR>:edit!<CR>", { noremap = true, silent = true })
 
 -- Tree
 keymap("n", "<C-b>", api.tree.toggle, { desc = "Toggle file explorer" })
@@ -18,14 +21,9 @@ keymap("n", "<leader>e", api.tree.focus, { desc = "Change focus to file explorer
 
 -- General
 keymap("n", "<leader>qq", "<cmd>q!<CR>", { desc = "Force quit" })
-keymap("n", "<leader>w",  "<cmd>w<CR>",  { desc = "Save file" })
+keymap("n", "<leader>w", "<cmd>w<CR>", { desc = "Save file" })
 keymap("n", "<C-v>", '"+p', { desc = "Paste from system clipboard" })
-keymap("i", "<C-v>", '<C-r>+', { desc = "Paste from system clipboard (insert mode)" })
+keymap("i", "<C-v>", "<C-r>+", { desc = "Paste from system clipboard (insert mode)" })
 
 -- Diagnostics
-keymap(
-  "n",
-  "gl",
-  vim.diagnostic.open_float,
-  {desc = "Open Diagnostics in Float"}
-)
+keymap("n", "gl", vim.diagnostic.open_float, { desc = "Open Diagnostics in Float" })
