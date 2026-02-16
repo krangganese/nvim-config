1cce80dcdf4e9fb84a6941c147611298265afd41
Dandi Pratama <mas.gondronglabanan@gmail.com>
2026-02-16 14:59:04 +0700

    feat: hide some directory from fzflua list

--- lua/plugins/fzflua.lua
+++ lua/plugins/fzflua.lua
@@ -1,5 +1,12 @@
 return {
-  "ibhagwan/fzf-lua",
-  dependencies = { "nvim-mini/mini.icons" },
-  opts = {}
+	"ibhagwan/fzf-lua",
+	dependencies = { "nvim-mini/mini.icons" },
+	opts = {
+		files = {
+			rg_opts = "--files --hidden -g !.git -g !node_modules -g !dist",
+		},
+		grep = {
+			rg_opts = "--column --line-number --no-heading --color=always --smart-case --hidden -g !.git -g !node_modules -g !dist",
+		},
+	},
 }
